71f961941f41b6a85dafbad373602beeb928b80d
Roman Paukner <romanpauk@gmail.com>
2024-03-07 23:20:22 +0100

    LICENSE

--- test/atomic.cpp
+++ test/atomic.cpp
@@ -2,7 +2,7 @@
 // This file is part of containers project <https://github.com/romanpauk/containers>
 //
 // See LICENSE for license and copyright information
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 //
 
 #include <containers/lockfree/atomic.h>
